aee98707bda473965e0dd795e625a9e3820648be
seossine <seossine@gmail.com>
2025-10-03 23:27:02 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,8 @@
-import 'package:flutter/foundation.dart' show kIsWeb;
 import 'package:flutter/material.dart';
 import 'package:rebloom/theme.dart';
 import 'package:rebloom/screens/home_page.dart';
-import 'package:webview_flutter_platform_interface/webview_flutter_platform_interface.dart';
-import 'package:webview_flutter_web/webview_flutter_web.dart';
 
 void main() {
-  if (kIsWeb) {
-    WebViewPlatform.instance = WebWebViewPlatform();
-  }
   runApp(const MyApp());
 }
 
